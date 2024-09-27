f578dabd03b4bacb3ffda37ce6a784e3b0884661
BoluAduloju <boluwatifeaduloju2@gmail.com>
2024-09-27 12:58:26 +0100

    fix: edited contact component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero"
 import TechStack from "./components/TechStack"
 import Experience from "./components/Experience"
 import Projects from "./components/Projects"
-import Contact from "./components/Contact.Jsx"
+import Contact from "./components/Contact"
 
 function App() {
   
