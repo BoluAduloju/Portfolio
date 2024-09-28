94510a9a19d302141e52550c4b8c5642036254b0
BoluAduloju <boluwatifeaduloju2@gmail.com>
2024-09-28 10:49:47 +0100

    fix: edited resume link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <div className="flex flex-shrink-0 items-center">
         <a href="/" aria-label="Home" className="mx-2 font-medium text-stone-200">BoluAduloju.</a>
       </div>
-      <a href="/BoluAduloju-Resume.pdf" target="_blank" rel="noopener noreferrer" download className="bg-stone-50 rounded-md px-3 py-2 text-sm text-stone-800 hover:bg-stone-200">Resume</a>
+      <a href="https://drive.google.com/file/d/1KCqyzadWk5Bje-zvMxUjCrAuwdILmjyY/view" target="_blank" rel="noopener noreferrer" download className="bg-stone-50 rounded-md px-3 py-2 text-sm text-stone-800 hover:bg-stone-200">Resume</a>
    </nav>
   )
 }
