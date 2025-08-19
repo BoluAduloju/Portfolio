94ba6004c4c8a5b4c49d64bee0ea380d05ba47db
boluaduloju <boluwatifeaduloju2@gmail.com>
2025-08-19 21:12:18 +0100

    fix: deployment error

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,3 @@
-import { i } from "framer-motion/client";
 import project1 from "../assets/projects/project1.png";
 import project2 from "../assets/projects/project2.png";
 import project3 from "../assets/projects/project3.png";
